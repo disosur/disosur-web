1d822f51b2c9ebd62ddb6ccf896b7f59fada095b
Alyssair Renhee Nikolai Morales <alymond.business@outlook.com>
2024-08-06 18:59:42 +0800

    Added first step in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,33 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
 export default function Home() {
   return (
-    <main className="flex flex-col justify-between items-center p-24 min-h-screen"></main>
+    <main className="h-screen">
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel className="p-8">
+          <section className="flex flex-col min-w-fit">
+            <h1 className="text-9xl font-semibold">Disosur Studio</h1>
+
+            <div className="flex flex-col space-y-4">
+              <p className="text-2xl">
+                A Fullstack dev who has a knack for Design sometimes
+              </p>
+              <p>
+                Hamlo, I&apos;m Alysstair. I make things using code and design
+                things with paper and pen (not figma). I write Typescript for a
+                living. But I like coding things with Go, Rust, and Flutter
+              </p>
+            </div>
+          </section>
+          <nav className="flex p-4 text-xl">Blogs Works About</nav>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel>Two</ResizablePanel>
+      </ResizablePanelGroup>
+    </main>
   );
 }
